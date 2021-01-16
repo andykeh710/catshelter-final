aaf20e01dff0529b71422825255698007f6bbd98
andyk <andy@example.com>
2021-01-16 16:39:27 -0600

    morewokring

--- routes/addBreed.js
+++ routes/addBreed.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('add-breed', { title: 'FROGS breed form' });
+  res.render('add-breed', { title: 'FROGS breed form' }); /// add breed is the hbs template name, the rest is the objec to pass in 
 });
 
 module.exports = router;
