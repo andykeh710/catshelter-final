4c246c2a07016601403d548344d211507ca8f0f0
andyk <andy@example.com>
2021-01-17 12:06:29 -0600

    new

--- routes/addBreed.js
+++ routes/addBreed.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('add-breed', { title: 'FROGS breed form' }); /// add breed is the hbs template name, the rest is the objec to pass in 
 });
 
+
+
+router.post('/', (req, res, next) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    let newbreed = req.body.breed;
+    let currentBreeds = JSON.parse(data)
+    fs.readFile
+})
+
+
+
+
+
+
+
+
+
 module.exports = router;
